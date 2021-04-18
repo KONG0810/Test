b30b7f881a12057e5f5bf2176d80377655ebaadd
Panhungting <a0910827893@gmail.com>
2021-04-19 00:52:28 +0900

    honest-system-base

---
+++ src/main/java/com/koiwaLearning/api/controller/honest/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.koiwaLearning.api.controller.honest;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/honest/employee")
+public class EmployeeController {
+
+    /**
+     * 跳转
+     */
+    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+    public ModelAndView index(Model model) {
+
+        System.out.println("welcome to honest employeeController");
+
+        // model.addAttribute("honest", "honest");
+        // model.addAttribute("title", "honest信息");
+        return new ModelAndView("honest/employee", "honestModel", model);
+    }
+}
